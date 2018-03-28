60798bcdaef7d8161c634ca9d4d5830db2ef6d4c
Kinnunen Eero TTK17SP <eerokinnunen@kamk.fi>
2018-03-28 17:06:52 +0300

    Grid still works.

--- Assets/Scripts/SceneScripts/LayoutManager.cs
+++ Assets/Scripts/SceneScripts/LayoutManager.cs
@@ -6,8 +6,11 @@ public class LayoutManager : MonoBehaviour
 {
 
     public Vector3[,] positions;
+    public GameObject[,] testGrid;
     [SerializeField]
     bool emptyGridDone = false;
+
+    public Color cantBuild;
 
     public List<GameObject> lakes;
     public int rndLakes = 1;
@@ -23,6 +26,7 @@ public class LayoutManager : MonoBehaviour
 
     public GameObject emptyGo;
     public GameObject lakeGo;
+    public GameObject treeGo;
     public GameObject centerGo;
 
     //https://docs.unity3d.com/ScriptReference/GL.html
@@ -41,6 +45,7 @@ public class LayoutManager : MonoBehaviour
     void CreateGrid()
     {
         positions = new Vector3[gridWidth, gridHeigth];
+        testGrid = new GameObject[gridWidth, gridHeigth];
 
         float centerOffsetX = gridWidth * tileWidth / 2 - tileWidth / 2;
         float centerOffsetY = 0;
@@ -51,29 +56,14 @@ public class LayoutManager : MonoBehaviour
             for (int y = 0; y < gridHeigth; y++)
             {
 
-                positions[x, y] = new Vector3 (transform.position.x - centerOffsetX + (1f * x) + (1f * y), transform.position.y - centerOffsetY + (0.5f * x) - (0.5f * y), 0);
+                positions[x, y] = new Vector3(transform.position.x - centerOffsetX + (1f * x) + (1f * y), transform.position.y - centerOffsetY + (0.5f * x) - (0.5f * y), 0);
 
-                //positions.Add(Instantiate(emptyGo, new Vector3(transform.position.x - centerOffsetX + (1f * y) + (currentWidth * 1f), transform.position.y - centerOffsetY + (0.5f * y) - (currentWidth * 0.5f)), Quaternion.identity));
-                //positions[x, y] = new Vector3(transform.position.x - centerOffsetX + (1f * y) + (currentWidth * 1f), transform.position.y - centerOffsetY + (0.5f * y) - (currentWidth * 0.5f), 0);
                 Vector3 newPos = new Vector3(positions[x, y].x, positions[x, y].y, transform.position.z);
-                GameObject newGo = Instantiate(emptyGo, newPos, Quaternion.identity);
-                newGo.name = x + " , " + y;
-
-
-                //positionSit.Add(0);
-
-
-                //if (currentWidth < gridHeigth)
-                //{
-                //    currentWidth += 1;
-                //}
-                //else
-                //{
-                //    currentWidth = 0;
-                //}
+                testGrid[x, y] = Instantiate(emptyGo, newPos, Quaternion.identity);
+                testGrid[x, y].name = x + " , " + y;
             }
         }
-        SpawnStructure(centerGo, gridWidth / 2, gridHeigth / 2, new Vector2(2,2));
+        SpawnStructure(centerGo, gridWidth / 2, gridHeigth / 2, new Vector2(2, 2));
         RandomGen();
     }
 
@@ -89,16 +79,27 @@ public class LayoutManager : MonoBehaviour
             tileCap = 0;
         }
 
-
-
         //NEW GRID SEED
         if (Input.GetKeyDown("space"))
         {
-            for (int i = 0; i < lakes.Count; i++)
+            //for (int i = 0; i < lakes.Count; i++)
+            //{
+            //    Destroy(lakes[i]);
+            //}
+            //RandomGen();
+            Application.LoadLevel("testscene");
+
+        }
+        //TEST GRID UPDATE
+        for (int x = 0; x < positions.GetLength(0); x++)
+        {
+            for (int y = 0; y < positions.GetLength(1); y++)
             {
-                Destroy(lakes[i]);
+                if (positions[x, y].z == 1)
+                {
+                    testGrid[x, y].GetComponent<SpriteRenderer>().color = cantBuild;
+                }
             }
-            RandomGen();
         }
     }
 
@@ -111,37 +112,26 @@ public class LayoutManager : MonoBehaviour
             int rnd1 = Random.Range(0, positions.GetLength(0));
             int rnd2 = Random.Range(0, positions.GetLength(1));
 
-            StartCoroutine(RndLakes(rnd1, rnd2));
+            for (; positions[rnd1, rnd2].z == 1;)
+            {
+                rnd1 = Random.Range(0, positions.GetLength(0));
+                rnd2 = Random.Range(0, positions.GetLength(1));
+            }
 
-            //while(positions[rnd1, rnd2].z == 1)
-            //{
-            //    rnd1 = Random.Range(0, positions.GetLength(0));
-            //    rnd2 = Random.Range(0, positions.GetLength(1));
-            //}
+            lakes.Add(Instantiate(lakeGo, positions[rnd1, rnd2], Quaternion.identity));
+            positions[rnd1, rnd2].z = 1;
         }
     }
 
-    IEnumerator RndLakes(int rnd1, int rnd2)
-    {
-        for (; positions[rnd1, rnd2].z == 1; )
-        {
-            rnd1 = Random.Range(0, positions.GetLength(0));
-            rnd2 = Random.Range(0, positions.GetLength(1));
-        }
 
-        lakes.Add(Instantiate(lakeGo, positions[rnd1, rnd2], Quaternion.identity));
-        positions[rnd1, rnd2].z = 1;
-
-        yield return null;
-    }
 
     private void SpawnStructure(GameObject structure, int posX, int posY, Vector2 size)
     {
         //SPAWN THE OBJECT
-        Vector3 newPosition = new Vector3(positions[posX, posY].x, positions[posX, posY].y - size.y/4, transform.position.z);
+        Vector3 newPosition = new Vector3(positions[posX, posY].x, positions[posX, posY].y, transform.position.z);
         GameObject obj = Instantiate(centerGo, newPosition, Quaternion.identity) as GameObject;
         //GET THE SIZE THAT STRUCTURE NEEDS
-   
+
         //Vector2 size = obj.GetComponent<HouseScript>().sizeOnGrid;
 
         //SET TILES TO TAKE
