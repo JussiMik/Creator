d3b6bd5390e9ac81a7f78864479c85973d749999
Mikkonen Jussi TTK17SP <jussimikkonen1@kamk.fi>
2018-04-06 13:58:38 +0300

    Basic pathfinding done, roaming monks still WIP

---
+++ Assets/Scripts/Pathfinding/PathfindingTargetLocation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindingTargetLocation : MonoBehaviour
+{
+    public bool moveToPosition;
+    public LayerMask unwalkable;
+    int layer;
+    float xPosition;
+    float yPosition;
+    Vector2 pos;
+    [SerializeField]
+    float newTargetLocationTimer;
+
+    // Use this for initialization
+    void Start()
+    {
+        unwalkable = LayerMask.GetMask("Unwalkable");
+        newTargetLocationTimer = Random.Range(5f, 15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  newTargetLocationTimer -= Time.deltaTime;
+
+        if (newTargetLocationTimer <= 0)
+        {
+            MoveToNewLocation();
+        }
+    }
+
+    void MoveToNewLocation()
+    {
+        xPosition = Random.Range(-5f, 5f);
+        yPosition = Random.Range(-5f, 5f);
+        pos = new Vector2(xPosition, yPosition);
+        transform.position = pos;
+        newTargetLocationTimer = Random.Range(5f, 15f);
+        moveToPosition = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag =="Building")
+        {
+            moveToPosition = false;
+            Debug.Log("Pathfinding target on unwalkable area");
+        }
+        else
+        {
+            moveToPosition = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Pathfinding/_TempMonk.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _TempMonk : MonoBehaviour
+{
+
+    public Transform targetTransform;
+    public GameObject targetObject;
+    public bool checkForNewDestination;
+    public float speed = 20;
+    Vector2[] path;
+    int targetIndex;
+
+    void Start()
+    {
+        //  StartCoroutine(RefreshPath());
+        InvokeRepeating("CheckForNewDestination", 0.5f, 1.5f);
+    }
+
+    void Update()
+    {
+        if (checkForNewDestination == true)
+        {
+            RefreshPath();
+            checkForNewDestination = false;
+        }
+    }
+    void CheckForNewDestination()
+    {
+        checkForNewDestination = targetObject.GetComponent<PathfindingTargetLocation>().moveToPosition;
+    }
+
+    private void RefreshPath()
+    {
+        Vector2 targetPositionOld = (Vector2)targetTransform.position + Vector2.up; // ensure != to target.position initially
+
+        if (true)
+        {
+            if (targetPositionOld != (Vector2)targetTransform.position)
+            {
+                targetPositionOld = (Vector2)targetTransform.position;
+
+                path = Pathfinding.RequestPath(transform.position, targetTransform.position);
+                StopCoroutine("FollowPath");
+                StartCoroutine("FollowPath");
+            }
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        if (path.Length > 0)
+        {
+            targetIndex = 0;
+            Vector2 currentWaypoint = path[0];
+
+            while (true)
+            {
+                if ((Vector2)transform.position == currentWaypoint)
+                {
+                    targetIndex++;
+                    if (targetIndex >= path.Length)
+                    {
+                        yield break;
+                    }
+                    currentWaypoint = path[targetIndex];
+                }
+
+                transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+                yield return null;
+
+            }
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube((Vector3)path[i], Vector3.one *.5f);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
