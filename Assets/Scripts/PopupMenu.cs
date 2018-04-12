e94d7a7a2c4173979281baf66aed356d3f0740e2
Mikkonen Jussi TTK17SP <jussimikkonen1@kamk.fi>
2018-04-12 14:53:47 +0300

    Started implementing pop-up window

---
+++ Assets/Scripts/PopupMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupMenu : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pos), Vector2.zero);
+            
+            if (hitInfo)
+            {
+                Debug.Log(hitInfo.transform.gameObject.name);
+                
+            }
+        }
+
+    }
+}
