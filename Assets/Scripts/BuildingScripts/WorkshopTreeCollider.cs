0138d0214c9b436fff6be1852d859c06111db149
Mikkonen Jussi TTK17SP <jussimikkonen1@kamk.fi>
2018-04-26 12:56:45 +0300

    Made workshop and quarry work with pathfinding

---
+++ Assets/Scripts/BuildingScripts/WorkshopTreeCollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkshopTreeCollider : MonoBehaviour
+{
+
+    public float totalTreeAmount;
+    public bool woodTimerCollision;
+    WoodWorkshopCS parent;
+    // Use this for initialization
+    void Start()
+    {
+        parent = transform.parent.gameObject.GetComponent<WoodWorkshopCS>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Tree")
+        {
+            totalTreeAmount++;
+            parent.SendMessage("UpdateValues", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Tree")
+        {
+            woodTimerCollision = true;
+            parent.SendMessage("UpdateValeus", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
